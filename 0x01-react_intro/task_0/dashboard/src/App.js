ec8aee45a214bc26af11356a8f9c70b7729c6b92
cheronodaisy <jepchumbadaisy96@gmail.com>
2024-01-20 16:56:47 +0300

    Modify the App

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,19 +1,27 @@
-import logo from './Holberton Logo.jpg';
-import './App.css';
+import logo from "./Holberton Logo.jpg";
+import "./App.css";
 import { getFullYear, getFooterCopy } from "./utils";
-
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header>
-      <body className="App-body">
+      </div>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
-      </body>
-      <footer className="App-footer">Copyright {getFullYear()} - {getFooterCopy()}</footer>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
+      </div>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
     </div>
   );
 }
